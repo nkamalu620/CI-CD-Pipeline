4d4c65c8adc0483c247b67ed6d1de6f011c0fc4f
nkamalu620 <nkamalu96@gmail.com>
2025-03-19 12:30:42 -0400

    updated paths for static files

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,13 +1,16 @@
 import express from 'express';
-// import path from 'node:path';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serves static files in the entire client's dist folder
-app.use(express.static('../../client/dist'));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 app.use(routes);
 db.once('open', () => {
     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
-// import path from 'node:path';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import db from './config/connection.js';
 import routes from './routes/index.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../../client/dist'));
+app.use(express.static(path.join(__dirname,'../../client/dist')));
 
 app.use(routes);
 
