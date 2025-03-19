2434e2a4d1ad9385080aad11b705d285ae32046f
nkamalu620 <nkamalu96@gmail.com>
2025-03-19 12:12:40 -0400

    updated gitignore, deleted unecessary file

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -2,9 +2,9 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 export default async (modelName, collectionName) => {
     try {
-        let modelExists = await models[modelName].db.db.listCollections({
+        let modelExists = await models[modelName]?.db?.db?.listCollections({
             name: collectionName
-        }).toArray();
+        })?.toArray() || [];
         if (modelExists.length) {
             await db.dropCollection(collectionName);
         }
--- server/dist/server.js
+++ server/dist/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static('../../client/dist'));
 app.use(routes);
 db.once('open', () => {
     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
